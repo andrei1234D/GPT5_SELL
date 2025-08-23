b28b986f57715429d5190febd4b41db8edff5642
Balas Marius Andrei <balasandrei200@yahoo.com>
2025-08-23 10:25:04 +0000

    keep_alive

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from threading import Thread
+import os
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Bot is alive!"
-
-def run():
-    app.run(host="0.0.0.0", port=8080)
+    return "✅ Bot is running!"
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    port = int(os.environ.get("PORT", 8080))  # use Replit’s assigned port if available
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    keep_alive()
